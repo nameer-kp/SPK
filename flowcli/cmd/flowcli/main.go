73cfff1bafb2760b274a1b8b83b6e8d9884f00a9
Nameer KP <kpnameer@gmail.com>
2026-02-02 09:24:47 +0530

    feat(flowcli): add Bubble Tea and core dependencies
    Add TUI framework and utility dependencies:
    - bubbletea, lipgloss, bubbles: TUI framework components
    - yaml.v3: YAML parsing for workflows
    - expr: Expression evaluation for conditions
    - godotenv: .env file loading
    - sqlx: Database operations

--- flowcli/cmd/flowcli/main.go
+++ flowcli/cmd/flowcli/main.go
@@ -3,6 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+
+	// TUI framework
+	_ "github.com/charmbracelet/bubbletea"
+	_ "github.com/charmbracelet/bubbles/list"
+	_ "github.com/charmbracelet/bubbles/spinner"
+	_ "github.com/charmbracelet/bubbles/textinput"
+	_ "github.com/charmbracelet/lipgloss"
+
+	// Utilities
+	_ "github.com/expr-lang/expr"
+	_ "github.com/jmoiron/sqlx"
+	_ "github.com/joho/godotenv"
+	_ "gopkg.in/yaml.v3"
 )
 
 func main() {
